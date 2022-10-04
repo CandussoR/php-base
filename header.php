12ed2aab282827cea446fd488efdc5a270ce3061
CandussoR <r.candusso@protonmail.com>
2022-10-04 14:17:02 +0200

    1.4-1 page contact et modifs page index et navigation header

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
         <ol>
             <li><a href="index.php?page=profile">Profil</a></li>
             <li><a href="index.php?page=hobbies">Hobby</a></li>
+            <li><a href="index.php?page=contact">Contact</a></li>
         </ol>
     </nav>
     <main class="main">
--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
 if (isset($_GET["page]"])) {
-    $requested_page = $_GET["page"];
-}
-else {
-    $requested_page = 'profile';
-}
-
-switch ($_GET["page"]) {
-    case 'profile':
-        include 'profile.php';
-        break;
-    case 'hobbies':
-        include 'hobbies.php';
-        break;
-    default:
-        include 'default.php';
-        break;
+    switch ($_GET["page"]) {
+        case 'profile':
+            include 'profile.php';
+            break;
+        case 'hobbies':
+            include 'hobbies.php';
+            break;
+        case 'contact':
+            include 'contact.php';
+            break;
+        default:
+            include 'default.php';
+            break;
+    }
+} else {
+    include 'default.php';
 }
 ?>
